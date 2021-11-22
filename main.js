21e6d94f9a8b489b646655572359f6a9cf73c2fd
Eddie Andersson <eddie.andersson@medieinstitutet.se>
2021-11-22 14:41:11 +0100

    version3

--- main.js
+++ main.js
@@ -1,28 +1,28 @@
 console.log("hej")
 
 let totalSum = []
+let posArray = []
+let negArray = []
+
 
 function btnAdd() {
-
     let posSum = 0;
     let negSum = 0;
-
+    let totalAmount=0;
     let inputValue = document.querySelector("#inp").value
     if (inputValue >= 0) {
-    console.log("positive")
     let newElement = document.createElement("li")
     let income = document.querySelector("#income")
-    income.appendChild(newElement)
-    //newElement.setAttribute("class", "li")    
+    income.appendChild(newElement)    
     let text = document.createTextNode(inputValue)
     newElement.appendChild(text)
     totalSum.push(inputValue)
+    posArray.push(inputValue)
 
-    totalSum.map((e)=> {
-        posSum += parseFloat(e)
-    })
+    for (let i = 0; i < posArray.length; i++) {
+        posSum += Number(posArray[i]);
+    }
 
-    console.log(posSum)
     document.querySelector("#outputOne").textContent = posSum;
 
     }
@@ -30,23 +30,25 @@ function btnAdd() {
         console.log("negative")
         let newElement = document.createElement("li")
         let expense = document.querySelector("#expense")
-        expense.appendChild(newElement)
-        //newElement.setAttribute("class", "li")    
+        expense.appendChild(newElement)    
         let text = document.createTextNode(inputValue)
         newElement.appendChild(text)
         totalSum.push(inputValue)
-        totalSum.map((e)=> {
-            negSum += parseFloat(e)
-        })
-    
-        console.log(negSum)
+        negArray.push(inputValue)    
+        for (let i = 0; i < negArray.length; i++) {
+            negSum += Number(negArray[i]);
+        }
         
         document.querySelector("#outputTwo").textContent = negSum;
 
     }
 
-    document.querySelector("#total").textContent = posSum + negSum;
+        for (let i = 0; i < totalSum.length; i++) {
+            totalAmount += Number(totalSum[i]);
+        }
+    
+    document.querySelector("#totalNumber").textContent = totalAmount
 
-}
+}   
 
 document.querySelector("#btn").addEventListener("click", btnAdd)
